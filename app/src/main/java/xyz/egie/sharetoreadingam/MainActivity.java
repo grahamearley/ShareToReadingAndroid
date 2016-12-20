acc709e0210d389bc674435b3be0b0eaa3f444e6
Graham Earley <graham.p.earley@gmail.com>
2016-12-20 16:06:05 -0600

    Separate settings onClick methods; fill in email field with stored value

--- app/src/main/java/xyz/egie/sharetoreadingam/MainActivity.java
+++ app/src/main/java/xyz/egie/sharetoreadingam/MainActivity.java
@@ -1,12 +1,17 @@
 package xyz.egie.sharetoreadingam;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     PreferencesManager prefs;
 
     private View hintWebLink;
+
+    private View saveEmailButton;
+    private EditText emailInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +35,39 @@ public class MainActivity extends AppCompatActivity {
         setHintOnClick();
 
 
-        final EditText emailInput = (EditText) findViewById(R.id.email_input);
-        TextView saveEmailButton = (TextView) findViewById(R.id.save_button);
+        this.emailInput = (EditText) findViewById(R.id.email_input);
+        this.saveEmailButton = (TextView) findViewById(R.id.save_button);
+
+        // Fill in the input field if there is an address already stored:
+        String storedEmail = prefs.getReadingEmail();
+        if (!TextUtils.isEmpty(storedEmail)) {
+            emailInput.setText(storedEmail);
+        }
+
+        setSaveButtonOnClick();
+        setEmailInputOnEnterKey();
+    }
+
+    private void setSaveButtonOnClick() {
         saveEmailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String readingEmail = emailInput.getText().toString();
                 prefs.setReadingEmail(readingEmail);
+                hideKeyboard();
+            }
+        });
+    }
+
+    private void setEmailInputOnEnterKey() {
+        emailInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEND) {
+                    saveEmailButton.performClick();
+                    return true;
+                }
+                return false;
             }
         });
     }
@@ -51,8 +85,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // TODO:
-    //  - ask for Reading.am email
-    //  - show link to the Reading page that gives the email
-
+    public void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(this);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
 }
