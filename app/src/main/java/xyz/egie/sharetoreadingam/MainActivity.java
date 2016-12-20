3d8355236707eb657cc294a9232a25cb799adfc1
Graham Earley <graham.p.earley@gmail.com>
2016-12-20 15:33:37 -0600

    Add field for adding Reading email, with hint link to the settings page

--- app/src/main/java/xyz/egie/sharetoreadingam/MainActivity.java
+++ app/src/main/java/xyz/egie/sharetoreadingam/MainActivity.java
@@ -1,14 +1,50 @@
 package xyz.egie.sharetoreadingam;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SETTINGS_EXTRAS_URL = "https://www.reading.am/settings/extras";
+    PreferencesManager prefs;
+
+    private View hintWebLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.prefs = PreferencesManager.getInstance(this);
+
+        this.hintWebLink = findViewById(R.id.email_hint);
+        setHintOnClick();
+
+
+        final EditText emailInput = (EditText) findViewById(R.id.email_input);
+        TextView saveEmailButton = (TextView) findViewById(R.id.save_button);
+        saveEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String readingEmail = emailInput.getText().toString();
+                prefs.setReadingEmail(readingEmail);
+            }
+        });
+    }
+
+    private void setHintOnClick() {
+        hintWebLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent hintIntent = new Intent(Intent.ACTION_VIEW);
+                hintIntent.setData(Uri.parse(SETTINGS_EXTRAS_URL));
+                startActivity(hintIntent);
+            }
+        });
     }
 
     // TODO:
